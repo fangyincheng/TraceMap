b2a26319f00e6122819392474ce1be1334151ebd
fangyincheng <fangyincheng@sina.com>
2016-12-22 04:30:45 +0800

    加入百度地图并实现定位

--- app/src/main/java/com/good/trace/tracemap/MainActivity.java
+++ app/src/main/java/com/good/trace/tracemap/MainActivity.java
@@ -1,13 +1,112 @@
 package com.good.trace.tracemap;
 
+import android.content.Context;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.location.BDLocation;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MapView mMapView;
+    private BaiduMap mBaiduMap;
+    private LocationClient mLocationClient;
+    private BDLocationListener mBDLocationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.bmapView);
+        mBaiduMap = mMapView.getMap();
+        //开启交通图
+        mBaiduMap.setTrafficEnabled(true);
+
+        mLocationClient = new LocationClient(getApplicationContext());
+        mBDLocationListener = new MyBDLocationListener();
+        // 注册监听
+        mLocationClient.registerLocationListener(mBDLocationListener);
+
+        // 声明定位参数
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);// 设置定位模式 高精度
+        option.setCoorType("bd09ll");// 设置返回定位结果是百度经纬度 默认gcj02
+        option.setScanSpan(500);// 设置发起定位请求的时间间隔 单位ms
+        option.setIsNeedAddress(true);// 设置定位结果包含地址信息
+        option.setNeedDeviceDirect(true);// 设置定位结果包含手机机头 的方向
+        // 设置定位参数
+        mLocationClient.setLocOption(option);
+        // 启动定位
+        mLocationClient.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 取消监听函数
+        if (mLocationClient != null) {
+            mLocationClient.unRegisterLocationListener(mBDLocationListener);
+        }
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+
+    private class MyBDLocationListener implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            // 非空判断
+            if (location != null) {
+                // 根据BDLocation 对象获得经纬度以及详细地址信息
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+
+                // 开启定位图层
+                mBaiduMap.setMyLocationEnabled(true);
+                // 构造定位数据
+                MyLocationData locData = new MyLocationData.Builder()
+                        .accuracy(location.getRadius())
+                        // 此处设置开发者获取到的方向信息，顺时针0-360
+                        .direction(location.getDirection()).latitude(latitude)
+                        .longitude(longitude).build();
+                // 设置定位数据
+                mBaiduMap.setMyLocationData(locData);
+                // 设置定位图层的配置（定位模式，是否允许方向信息，用户自定义定位图标）
+                MyLocationConfiguration config = new MyLocationConfiguration(MyLocationConfiguration.LocationMode.COMPASS, true, null);
+                mBaiduMap.setMyLocationConfigeration(config);
+
+                if (mLocationClient.isStarted()) {
+                    // 获得位置之后停止定位
+                    mLocationClient.stop();
+                }
+            }
+        }
     }
 }
